735e9efd210adf50efb8f1f19d3d416258c221e9
Sourav Palit <souravpalitrana@gmail.com>
2022-05-17 20:41:33 +0600

    Initial project setup for the leetcode daily challenge 17 may 2022

---
+++ TreesAndGraphs/FindCorrespondingNodeOfABinaryTreeInACloneOfThatTree/src/findcorrespondingnodeofabinarytreeinacloneofthattree/Solution.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findcorrespondingnodeofabinarytreeinacloneofthattree;
+
+/**
+ *
+ * @author souravpalit
+ */
+public class Solution {
+    
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        
+    }
+    
+}
