30a6b24783ef154ac72ffb6bd9174fdbbff81f39
Sourav Palit <souravpalitrana@gmail.com>
2022-05-17 20:49:48 +0600

    Add the solution of the problem Find a Corresponding Node of a Binary Tree in a Clone of That Tree

--- TreesAndGraphs/FindCorrespondingNodeOfABinaryTreeInACloneOfThatTree/src/findcorrespondingnodeofabinarytreeinacloneofthattree/Solution.java
+++ TreesAndGraphs/FindCorrespondingNodeOfABinaryTreeInACloneOfThatTree/src/findcorrespondingnodeofabinarytreeinacloneofthattree/Solution.java
@@ -12,7 +12,25 @@ package findcorrespondingnodeofabinarytreeinacloneofthattree;
 public class Solution {
     
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null) {
+            return original;
+        }
         
+        if (original == target) {
+            return cloned;
+        }
+        
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        if (left != null) {
+            return left;
+        }
+        
+        TreeNode right = getTargetCopy(original.right, cloned.right, target);
+        if (right != null) {
+            return right;
+        }
+        
+        return null;
     }
     
 }
--- TreesAndGraphs/FindCorrespondingNodeOfABinaryTreeInACloneOfThatTree/src/findcorrespondingnodeofabinarytreeinacloneofthattree/TreeNode.java
+++ TreesAndGraphs/FindCorrespondingNodeOfABinaryTreeInACloneOfThatTree/src/findcorrespondingnodeofabinarytreeinacloneofthattree/TreeNode.java
@@ -10,5 +10,8 @@ package findcorrespondingnodeofabinarytreeinacloneofthattree;
  * @author souravpalit
  */
 public class TreeNode {
-    
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 }
