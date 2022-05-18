2e62de38a27513556033094cfb83cd499d0e1660
Sourav Palit <souravpalitrana@gmail.com>
2022-05-18 22:41:53 +0600

    Initial project setup of the leetcode daily challenge 18 may 2022

---
+++ TreesAndGraphs/CriticalConnectionsInANetwork/src/criticalconnectionsinanetwork/Solution.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package criticalconnectionsinanetwork;
+
+import java.util.List;
+
+/**
+ *
+ * @author souravpalit
+ */
+public class Solution {
+    
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        
+    }
+}
