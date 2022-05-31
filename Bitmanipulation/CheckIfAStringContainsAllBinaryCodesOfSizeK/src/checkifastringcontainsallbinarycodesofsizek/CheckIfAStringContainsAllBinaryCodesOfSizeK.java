d6ab42140a81f00c0177a5a81c681d3e19ec0fb8
Sourav Palit <souravpalitrana@gmail.com>
2022-05-31 22:47:30 +0600

    Add the leetcode daily challenge on 31 May 2022
    Check If a String Contains All Binary Codes of Size K

---
+++ Bitmanipulation/CheckIfAStringContainsAllBinaryCodesOfSizeK/src/checkifastringcontainsallbinarycodesofsizek/CheckIfAStringContainsAllBinaryCodesOfSizeK.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkifastringcontainsallbinarycodesofsizek;
+
+/**
+ *
+ * @author souravpalit
+ */
+public class CheckIfAStringContainsAllBinaryCodesOfSizeK {
+
+    public static boolean hasAllCodes(String s, int k) {
+        int need = 1 << k;
+        boolean[] got = new boolean[need];
+        int allOne = need - 1;
+        int hashVal = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // calculate hash for s.substr(i-k+1,i+1)
+            hashVal = ((hashVal << 1) & allOne) | (s.charAt(i) - '0');
+            // hash only available when i-k+1 > 0
+            if (i >= k - 1 && !got[hashVal]) {
+                got[hashVal] = true;
+                need--;
+                if (need == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+}
