54032e5d32b3872730f8df61ded7de8a4d535e79
Sourav Palit <souravpalitrana@gmail.com>
2022-06-02 00:04:44 +0600

    Add the solution

--- TreesAndGraphs/ConstructBinaryTreefromPreorderAndInorderTraversal/src/constructbinarytreefrompreorderandinordertraversal/ConstructBinaryTreefromPreorderAndInorderTraversal.java
+++ TreesAndGraphs/ConstructBinaryTreefromPreorderAndInorderTraversal/src/constructbinarytreefrompreorderandinordertraversal/ConstructBinaryTreefromPreorderAndInorderTraversal.java
@@ -5,17 +5,47 @@
  */
 package constructbinarytreefrompreorderandinordertraversal;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author souravpalit
  */
 public class ConstructBinaryTreefromPreorderAndInorderTraversal {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    int preOrderIdx;
+    Map<Integer, Integer> inorderIdxMap;
+    
+    // Time Complexity: O(n) where n is the number of elements
+    // Space Complexity: O(n) where n is the number of elements
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        inorderIdxMap = new HashMap<>();
+        preOrderIdx = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            inorderIdxMap.put(inorder[i], i);
+        }
+        
+        return generateTree(preorder, inorder, 0, inorder.length - 1);
     }
     
+    public TreeNode generateTree(
+            int[] preorder, 
+            int[] inorder,
+            int low,
+            int high
+                    
+    ) {
+        if (low > high) {
+            return null;
+        }
+        
+        TreeNode currentNode = new TreeNode(preorder[preOrderIdx++]);
+        int mid = inorderIdxMap.get(currentNode.val);
+        currentNode.left = generateTree(preorder, inorder, low, mid - 1);
+        currentNode.right = generateTree(preorder, inorder, mid + 1, high);
+        return currentNode;
+    }
+
 }
