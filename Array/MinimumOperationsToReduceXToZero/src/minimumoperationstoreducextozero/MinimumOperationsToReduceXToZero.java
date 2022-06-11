262a43ded2516f0663db4401cbd566bb35bc441c
Sourav Palit <souravpalitrana@gmail.com>
2022-06-11 23:56:59 +0600

    Initial project setup for Minimum Operations to Reduce X to Zero

---
+++ Array/MinimumOperationsToReduceXToZero/src/minimumoperationstoreducextozero/MinimumOperationsToReduceXToZero.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimumoperationstoreducextozero;
+
+/**
+ *
+ * @author souravpalit
+ */
+public class MinimumOperationsToReduceXToZero {
+
+    public int minOperations(int[] nums, int x) {
+        
+    }
+    
+}
