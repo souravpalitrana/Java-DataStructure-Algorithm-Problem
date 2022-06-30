467d388f022c991ad16ce7d3f872ae0b3cf1e377
Sourav Palit <souravpalitrana@gmail.com>
2022-06-30 11:20:17 +0600

    Add the leetcode daily challenge solution of 30 June 2022

---
+++ Array/MinimumMovesToEqualArrayElementsII/src/minimummovestoequalarrayelementsii/MinimumMovesToEqualArrayElementsII.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimummovestoequalarrayelementsii;
+
+import java.util.Arrays;
+
+/**
+ * Leetcode: 462. Minimum Moves to Equal Array Elements II
+ * Problem Link: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+ * @author souravpalit
+ */
+public class MinimumMovesToEqualArrayElementsII {
+
+    // TC: O(nlogn) SC: O(1)
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int mid = ((nums.length - 1) / 2);
+        mid = nums[mid];
+        int movesCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            movesCount += Math.abs(mid - nums[i]);
+        }
+        
+        return movesCount;
+    }
+}
