163a74b3296a218d7084f3dd5a7fcbdb87232f00
Sourav Palit <souravpalitrana@gmail.com>
2022-07-04 13:00:48 +0600

    Add solution

--- Math/BasicCalculatorII/src/basiccalculatorii/BasicCalculatorII.java
+++ Math/BasicCalculatorII/src/basiccalculatorii/BasicCalculatorII.java
@@ -5,17 +5,50 @@
  */
 package basiccalculatorii;
 
+import java.util.Stack;
+
 /**
- *
+ * Leetcode Problem: Basic Calculator II
+ * Problem Link: https://leetcode.com/problems/basic-calculator-ii/
  * @author souravpalit
  */
 public class BasicCalculatorII {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    // TC: O(n) SC: O(n)
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operator = '+';
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                currentNumber = currentNumber * 10 + (ch - '0');
+            }
+            
+            if (!Character.isDigit(ch) && 
+                !Character.isWhitespace(ch) || 
+                i == s.length() - 1) {
+                if (operator == '+') {
+                    stack.push(currentNumber);
+                } else if (operator == '-') {
+                    stack.push(-currentNumber);
+                } else if (operator == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                } else if (operator == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operator = ch;
+                currentNumber = 0;
+            }
+        }
+        
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        
+        return result;
     }
     
 }
