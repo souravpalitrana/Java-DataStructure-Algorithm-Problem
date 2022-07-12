d0f781345c214ec9705296179b6a30829e2595cd
Sourav Palit <souravpalitrana@gmail.com>
2022-07-12 20:02:53 +0600

    Add the solution of the daily challenge on 12 July 2022

---
+++ DynamicProgramming/MatchsticksToSquare/src/matchstickstosquare/MatchsticksToSquare.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matchstickstosquare;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author souravpalit
+ */
+public class MatchsticksToSquare {
+    
+    public static void main(String args[]) {
+        int [] sticks = {5,5,5,5,4,4,4,4,3,3,3,3};
+        System.out.println(makesquare(sticks));
+    }
+
+    static int [] sides;
+    static int eachSideLength;
+    
+    public static boolean makesquare(int [] matchSticks) {
+        sides = new int [4];
+        int totalLength = 0;
+        eachSideLength = Integer.MIN_VALUE;
+        List<Integer> sticks = new ArrayList<>();
+        for (int stickLength : matchSticks) {
+            totalLength += stickLength;
+            sticks.add(stickLength);
+        }
+        
+        int eachSideLength = totalLength / 4;
+        if (eachSideLength * 4 != totalLength) {
+            return false;
+        }
+        
+        Collections.sort(sticks, Collections.reverseOrder());
+        return explore(sticks, 0);
+    }
+    
+    public static boolean explore(List<Integer> matchSticks, int stickIdx) {
+        if (stickIdx == matchSticks.size()) {
+            return sides[0] == sides[1] && sides[1] == sides[2] && sides[2] == sides[3];
+        }
+        
+        int stickLength = matchSticks.get(stickIdx);
+        
+        for (int i = 0; i < 4; i++) {
+            int currentLength = sides[i] + stickLength;
+            if (sides[i] + stickLength <= eachSideLength) {
+                sides[i] += stickLength;
+                if (explore(matchSticks, stickIdx + 1)) {
+                    return true;
+                }
+                sides[i] -= stickLength;
+            }
+        }
+        
+        return false;
+    }
+    
+}
