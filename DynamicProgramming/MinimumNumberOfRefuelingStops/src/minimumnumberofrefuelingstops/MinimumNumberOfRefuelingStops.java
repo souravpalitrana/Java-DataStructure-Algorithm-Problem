13b8de36cba3f4e4dd372000eb1692d6cd0c44fc
Sourav Palit <souravpalitrana@gmail.com>
2022-08-20 23:26:00 +0600

    Add the project Minimum Number of Refueling Stops

---
+++ DynamicProgramming/MinimumNumberOfRefuelingStops/src/minimumnumberofrefuelingstops/MinimumNumberOfRefuelingStops.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimumnumberofrefuelingstops;
+
+/**
+ * Leetcode: 871. Minimum Number of Refueling Stops
+ * Problem Link: https://leetcode.com/problems/minimum-number-of-refueling-stops/
+ */
+public class MinimumNumberOfRefuelingStops {
+
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        
+    }
+    
+}
