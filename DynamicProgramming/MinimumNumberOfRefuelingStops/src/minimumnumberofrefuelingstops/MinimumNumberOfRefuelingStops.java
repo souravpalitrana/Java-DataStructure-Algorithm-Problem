b7aee8d1dbe2db955cb35bf6b4fdf71738e95ade
Sourav Palit <souravpalitrana@gmail.com>
2022-08-21 10:35:13 +0600

    Add the solution of the problem minimum number of refuelling stops

--- DynamicProgramming/MinimumNumberOfRefuelingStops/src/minimumnumberofrefuelingstops/MinimumNumberOfRefuelingStops.java
+++ DynamicProgramming/MinimumNumberOfRefuelingStops/src/minimumnumberofrefuelingstops/MinimumNumberOfRefuelingStops.java
@@ -5,14 +5,44 @@
  */
 package minimumnumberofrefuelingstops;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 /**
  * Leetcode: 871. Minimum Number of Refueling Stops
  * Problem Link: https://leetcode.com/problems/minimum-number-of-refueling-stops/
  */
 public class MinimumNumberOfRefuelingStops {
+    
+    public int minRefuelStops(int target, int tank, int[][] stations) {
+        // pq is a maxheap of gas station capacities
+        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
+        int ans = 0, prev = 0;
+        for (int[] station: stations) {
+            int location = station[0];
+            int capacity = station[1];
+            tank -= location - prev;
+            while (!pq.isEmpty() && tank < 0) {  // must refuel in past
+                tank += pq.poll();
+                ans++;
+            }
 
-    public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        
+            if (tank < 0) return -1;
+            pq.offer(capacity);
+            prev = location;
+        }
+
+        // Repeat body for station = (target, inf)
+        {
+            tank -= target - prev;
+            while (!pq.isEmpty() && tank < 0) {
+                tank += pq.poll();
+                ans++;
+            }
+            if (tank < 0) return -1;
+        }
+
+        return ans;
     }
     
 }
